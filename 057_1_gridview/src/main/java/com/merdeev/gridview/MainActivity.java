3950cded161c8b7ca6be1217b66b1b6c46f3affc
Ruslan Merdeev <rz.merdeev@itelma.su>
2017-09-28 12:24:25 +0400

    Added module: 057_1_gridview

---
+++ 057_1_gridview/src/main/java/com/merdeev/gridview/MainActivity.java
@@ -0,0 +1,37 @@
+package com.merdeev.gridview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+    GridView gvMain;
+    ArrayAdapter<String> adapter;
+    String[] data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        data = getResources().getStringArray(R.array.data);
+
+        adapter = new ArrayAdapter<String>(this, R.layout.item, R.id.tvText, data);
+        gvMain = (GridView) findViewById(R.id.gvMain);
+        gvMain.setAdapter(adapter);
+        adjustGridView();
+    }
+
+    private void adjustGridView() {
+        gvMain.setNumColumns(GridView.AUTO_FIT);
+        gvMain.setColumnWidth(60);
+        gvMain.setVerticalSpacing(5);
+        gvMain.setHorizontalSpacing(5);
+        gvMain.setStretchMode(GridView.NO_STRETCH);
+        //gvMain.setStretchMode(GridView.STRETCH_COLUNM_WIDTH); // default
+        //gvMain.setStretchMode(GridView.STRETCH_SPACING);
+        //gvMain.setStretchMode(GridView.STRETCH_SPACING_UNIFORM);
+    }
+}
